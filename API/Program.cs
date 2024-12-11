f6e536d430e0086032bc1d24aca6aa05d7253b52
vjgaur46 <vjgaur46@gmail.com>
2024-12-12 03:07:51 +0530

    End of section Create angular and set Https over angular localhost

--- API/Program.cs
+++ API/Program.cs
@@ -5,15 +5,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-
-
 builder.Services.AddDbContext<DataContext>(opt=>{
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost4200",
+        builder => builder.WithOrigins("http://localhost:4200")
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors("AllowLocalhost4200"); // Apply the CORS policy
 app.MapControllers();
 
 app.Run();
