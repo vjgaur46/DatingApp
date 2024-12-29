c9058c5d8636d5ee15cbb8b3a6eea5ba09f910dd
vjgaur46 <vjgaur46@gmail.com>
2024-12-29 19:05:40 +0530

    implemented the error handling

--- ClientUI/src/app/app.routes.ts
+++ ClientUI/src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { MemberDetailComponent } from './member/member-detail/member-detail.comp
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { authGuard } from './_guards/auth.guard';
+import { TestErrorComponent } from './errors/test-error/test-error.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 
 export const routes: Routes = [
     {path:'',component:HomeComponent},
@@ -18,6 +21,9 @@ export const routes: Routes = [
             {path:'lists',component:ListsComponent},
             {path:'messages',component:MessagesComponent},
         ]
-    },    
+    },   
+    {path:'errors',component:TestErrorComponent}, 
+    {path:'not-found',component:NotFoundComponent}, 
+    {path:'server-error',component:ServerErrorComponent}, 
     {path:'**',component:HomeComponent, pathMatch:'full'}
 ];
